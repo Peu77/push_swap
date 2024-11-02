0acc2177e66f6e7d6cf5e401d0cba1946c039ed9
peu77 <emilebert@icloud.com>
2024-11-02 17:18:06 +0100

    update makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 NAME = push_swap
 NAME_BONUS = checker
 CC = cc
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror -I $(FTPRINTF_DIR)/src -I $(FTPRINTF_DIR)/libft/ -fsanitize=address -g
+
+FTPRINTF_DIR = ft_printf
+FTPRINTF_LIB = $(FTPRINTF_DIR)/libftprintf.a
 
 SRC = push_swap.c
 SRC_BONUS = checker.c
@@ -11,18 +14,24 @@ OBJ_BONUS = $(SRC_BONUS:.c=.o)
 
 all: $(NAME)
 
-$(NAME): $(OBJ)
-	$(CC) $(CFLAGS) -o $(NAME) $(OBJ)
+$(NAME): $(OBJ) $(FTPRINTF_LIB)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJ) $(FTPRINTF_LIB)
 
 bonus: $(NAME_BONUS)
 
-$(NAME_BONUS): $(OBJ_BONUS)
-	$(CC) $(CFLAGS) -o $(NAME_BONUS) $(OBJ_BONUS)
+$(NAME_BONUS): $(OBJ_BONUS)  $(FTPRINTF_LIB)
+	$(CC) $(CFLAGS) -o $(NAME_BONUS) $(OBJ_BONUS) $(FTPRINTF_LIB)
+
+$(FTPRINTF_LIB):
+	make -C $(FTPRINTF_DIR)
 
 clean:
+	make -C $(FTPRINTF_DIR) clean
 	rm -f $(OBJ) $(OBJ_BONUS)
 
 fclean: clean
+	make -C $(FTPRINTF_DIR) fclean
+	rm -f $(NAME) $(NAME_BONUS)
 
 re: fclean all
 
--- push_swap.c
+++ push_swap.c
@@ -1,7 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+#include "libft.h"
+
+void  parse_args_to_stack(int argc, char **argv, t_list **stack_a)
 {
+    int i;
+    int j;
+    int* num;
+    char **split;
+
+    i = 1;
+    while (i < argc)
+    {
+        split = ft_split(argv[i], ' ');
+        j = 0;
+        while (split[j])
+        {
+            num = (int*)malloc(sizeof(int));
+            *num = ft_atoi(split[j]);
+            free(split[j]);
+            ft_lstadd_back(stack_a, ft_lstnew(num));
+            j++;
+        }
+        free(split);
+        i++;
+    }
+}
+
+void swap_first(t_list** stack_a, t_list** stack_b)
+{
+    // push first element of stack_a to stack_b
+
+    t_list* new_node = ft_lstnew((*stack_a)->content);
+    ft_lstadd_front(stack_b, new_node);
+    ft_lst
+}
+
+void print_stack(void* content)
+{
+    printf("%d\n", *(int*)content);
+}
+
+int main(int argc, char** argv)
+{
+    t_list *stack_a;
+    t_list *stack_b;
+
+    stack_a = NULL;
+    stack_b = NULL;
+
+    parse_args_to_stack(argc, argv, &stack_a);
+    swap_first( &stack_a, &stack_b);
+
+
+    printf("stack a:\n");
+    ft_lstiter(stack_a, print_stack);
+
+
+    printf("stack b:\n");
+    ft_lstiter(stack_b, print_stack);
+
     printf("Hello, World!\n");
     return 0;
 }
